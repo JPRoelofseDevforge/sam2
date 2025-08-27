dbb3f663174ce820196a5ddebcf6abe47800166e
JP Roelofse <jproelofse@gmail.com>
2025-08-27 11:10:33 +0200

    prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   assetsInclude: ['**/*.glb', '**/*.gltf'],
   optimizeDeps: {
     exclude: ['lucide-react', 'pg', 'dotenv'],
